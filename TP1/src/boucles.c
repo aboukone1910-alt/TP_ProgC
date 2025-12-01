8408921c0d9ffe7dfad1b7046b8d38b1dfc41fcd
aboukone1910-alt <aboukone1910@gmail.com>
2025-12-01 11:12:51 +0100

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,22 +1,11 @@
-
 #include <stdio.h>
 
-int main() {
-    int compteur = 5;    
-
-    if (compteur >= 10 || compteur <= 0) {
-        printf("Erreur : compteur doit être entre 1 et 9.\n");
-        return 1;
-    }
-
+void triangle_for(int compteur) {
     for (int i = 1; i <= compteur; i++) {
-
         for (int j = 1; j <= i; j++) {
-
             if (j == 1 || j == i) {
                 printf("* ");
             }
-            
             else if (i == compteur) {
                 printf("* ");
             }
@@ -26,26 +15,13 @@ int main() {
         }
         printf("\n");
     }
-
-    return 0;
 }
 
-#include <stdio.h>
-
-int main() {
-    int compteur = 5;    
-
-    if (compteur >= 10 || compteur <= 0) {
-        printf("Erreur : compteur doit être entre 1 et 9.\n");
-        return 1;
-    }
-
+void triangle_while(int compteur) {
     int i = 1;
     while (i <= compteur) {
-
         int j = 1;
         while (j <= i) {
-
             if (j == 1 || j == i) {
                 printf("* ");
             }
@@ -55,13 +31,28 @@ int main() {
             else {
                 printf("# ");
             }
-
             j++;
         }
-
         printf("\n");
         i++;
     }
+}
+
+int main() {
+    int compteur = 5;    
+
+    if (compteur >= 10 || compteur <= 0) {
+        printf("Erreur : compteur doit être entre 1 et 9.\n");
+        return 1;
+    }
+
+    printf("Triangle avec for :\n");
+    triangle_for(compteur);
+
+    printf("\nTriangle avec while :\n");
+    triangle_while(compteur);
 
     return 0;
 }
+
+
